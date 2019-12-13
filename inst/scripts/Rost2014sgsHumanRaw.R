7d509e5cd42da728c60949e65067d5f933b8f8fe
plantton <plantton@gmail.com>
2019-12-13 10:37:35 +0100

    Dec.13 Morning

--- inst/scripts/Rost2014sgsHumanRaw.R
+++ inst/scripts/Rost2014sgsHumanRaw.R
@@ -1,6 +1,20 @@
 library(MSnbase)
 library(BiocFileCache)
 library(HDF5Array)
+library(mzR)
+library(beachmat)
+library(Matrix)
 
 ## Set workdir as the current path
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+## Sed to load the MSnbase package and
+setMSnbaseVerbose(FALSE)
+
+## Set Raw Data Path
+rawMSPath <- "../../../RawMS/napedro_L120420_010_SW.mzXML.gz"
+
+## Read MS raw datasets as "MSnExp" instance (One Raw dataset)
+## https://lgatto.github.io/MSnbase/articles/v04-benchmarking.html
+rawMSdata <- readMSData(rawMSPath, msLevel. = 1L,
+                        mode = "onDisk")
+
