20188759ba61573be71d0826e688ea29d32965bf
plantton <plantton@gmail.com>
2019-12-17 23:09:07 +0100

    hide files

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  msg <- paste0("\nThis is msdata2 version ",
-                packageVersion("msdata2"), ".\n",
-                "Use 'msdata2()' to list available data sets.")
-  packageStartupMessage(msg)
-}
